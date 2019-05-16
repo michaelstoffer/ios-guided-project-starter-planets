1fdfe992c78cdba217360186ef93d7dcd2de320f
Michael Stoffer <mstoffer@michaelstoffer.com>
2019-05-15 22:11:15 -0400

    Added PlanetCollectionViewCell and setup interface for planets

---
+++ Planets/View Controllers/PlanetCollectionViewCell.swift
@@ -0,0 +1,14 @@
+//
+//  PlanetCollectionViewCell.swift
+//  Planets
+//
+//  Created by Michael Stoffer on 5/15/19.
+//  Copyright © 2019 Lambda Inc. All rights reserved.
+//
+
+import UIKit
+
+class PlanetCollectionViewCell: UICollectionViewCell {
+    @IBOutlet var imageView: UIImageView!
+    @IBOutlet var label: UILabel!
+}
